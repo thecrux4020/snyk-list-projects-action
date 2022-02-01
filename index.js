2a7af0b0fbdbcd3804efbac32635742b60c40dab
Leandro Mantovani <mantux@Leandros-MacBook-Pro.local>
2022-02-01 10:46:46 -0300

    fix error

--- index.js
+++ index.js
@@ -2,35 +2,40 @@ const core = require('@actions/core');
 const axios = require('axios')
 
 
-try {
+async function api_call() {
+    try {
 
-    const config = {
-        headers: {
-            "Authorization": 'token ' + core.getInput('api_token')
+        const config = {
+            headers: {
+                "Authorization": 'token ' + core.getInput('api_token')
+            }
+        }
+    
+        const response = await axios.post('https://snyk.io/api/v1/org/' + core.getInput('org_id') + '/projects', {
+            'filters': {
+                'name': core.getInput('project_name'),
+                'origin': core.getInput('origin')
+            }
+        }, config)
+    
+        if (response.status !== 200) { core.setFailed("Error. API response code: " + response.status) }
+    
+        const responseData = response.data
+    
+        let projects = []
+    
+        responseData.projects.forEach(project => {
+            projects.append(project.id)
+        });
+    
+        core.setOutput("project_ids", projects);
+    
+    } catch(err) {
+        if(axios.isAxiosError(err)) {
+            core.setFailed(err.message);
         }
     }
-
-    const response = await axios.post('https://snyk.io/api/v1/org/' + core.getInput('org_id') + '/projects', {
-        'filters': {
-            'name': core.getInput('project_name'),
-            'origin': core.getInput('origin')
-        }
-    }, config)
-
-    if (response.status !== 200) { core.setFailed("Error. API response code: " + response.status) }
-
-    const responseData = response.data
-
-    let projects = []
-
-    responseData.projects.forEach(project => {
-        projects.append(project.id)
-    });
-
-    core.setOutput("project_ids", projects);
-
-} catch(err) {
-    if(axios.isAxiosError(err)) {
-        core.setFailed(error.message);
-    }
+       
 }
+
+api_call()
